6979dfb558d4339b1750567b669abf9d8e134d33
Serhii Nesteruk <yarench228@gmail.com>
2024-02-10 00:57:39 +0100

    Add camera

--- assets/shaders/main.frag
+++ assets/shaders/main.frag
@@ -4,5 +4,5 @@ out vec4 FragColor;
 
 void main()
 {
-   FragColor = vec4(0.1f, 0.5f, 1.0f, 1.0f);
+   FragColor = vec4(0.f, 0.f, 1.f, 1.f);
 }
--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 #include "Window.h"
 #include "ShaderProgram.h"
 #include "Shader.h"
-#include "Texture.h"
 #include "VAO.h"
 #include "VBO.h"
 
-#include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
 #include <vector>
 
-
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 int main()
 {
-	Window window(Window::Size(800, 800), "Hi");
+	GLint screenWidth = 800.f, screenHeight = 800.f;
+	Window window(Window::Size(screenWidth, screenHeight), "Hi");
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -23,15 +23,48 @@ int main()
 		return -1;
 	}
 
-	std::vector<float> vertices = {
-		// first triangle
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f,  0.5f, 0.0f,  // top left
-		// second triangle
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left
+	std::vector<GLfloat> vertices = {
+		-0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f,  0.5f, -0.5f,
+		0.5f,  0.5f, -0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+
+		-0.5f, -0.5f,  0.5f,
+		0.5f, -0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
+
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+
+		0.5f,  0.5f,  0.5f,
+		0.5f,  0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
+
+		-0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f,  0.5f,
+		0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f, -0.5f,
+
+		-0.5f,  0.5f, -0.5f,
+		0.5f,  0.5f, -0.5f,
+		0.5f,  0.5f,  0.5f,
+		0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f
 	};
 
 	Shader vertexShader("assets//shaders//main.vert", Gl::Shader::Type::VERTEX);
@@ -42,19 +75,68 @@ int main()
 	VBO vbo(vertices);
 	VAO vao(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
 
+	glm::mat4 model = glm::mat4(1.0f);
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 projection = glm::perspective(45.0f, (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
+
 	while (!window.shouldClose())
 	{
+		window.pollEvents();
+
 		window.clearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		window.clear(GL_COLOR_BUFFER_BIT);
 
 		shaderProgram.use();
+
+		// camera
+		glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+
+		glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+		glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+		glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+
+		glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+		GLfloat radius = 10.0f;
+		GLfloat camX = sin(glfwGetTime()) * radius;
+		GLfloat camZ = cos(glfwGetTime()) * radius;
+		view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+
+
+		model = glm::rotate(glm::mat4(1.0f), -55.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+		//view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+
+		// load matrices to GPU
+		GLint modelLoc = glGetUniformLocation(shaderProgram.getProgram(), "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+		GLint viewLoc = glGetUniformLocation(shaderProgram.getProgram(), "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+		GLint projLoc = glGetUniformLocation(shaderProgram.getProgram(), "projection");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
 		vao.bind();
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		window.swapBuffers(window.getWinTarget());
-		window.pollEvents();
 	}
-
 
 	return 0;
 }
+
+//void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+//{
+//
+//	GLfloat cameraSpeed = 0.05f;
+//	if(key == GLFW_KEY_W)
+//		cameraPos += cameraSpeed * cameraFront;
+//	if(key == GLFW_KEY_S)
+//		cameraPos -= cameraSpeed * cameraFront;
+//	if(key == GLFW_KEY_A)
+//		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+//	if(key == GLFW_KEY_D)
+//		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+//}
